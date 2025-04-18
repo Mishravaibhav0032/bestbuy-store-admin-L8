ff5194110b5ab30394d0afcb6146e73547783538
Vaibhav Mishra <mish0032@algonquinlive.com>
2025-04-18 11:10:11 -0400

    Final Commit

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/bestbuy-logo.jpg" alt="BestBuy Logo">
       </a>
       Admin Portal
     </div>
